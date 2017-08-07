88ec50c31e4c8f14e2389c8aa5a4b864ad0c8ec7
susy <susy@Ctrip.com>
2017-08-07 17:43:19 +0800

    update comment

--- leetcode/283-moveZeroes/Solution.java
+++ leetcode/283-moveZeroes/Solution.java
@@ -8,6 +8,27 @@ public class Solution {
         solution.print(nums);
     }
 
+     /**
+     * 暴力解法:  循环两次，将为0的的数依次传递（冒泡）到末尾
+     * 浪费：i要一直循环到lenth-1;每次寻找非0元素，都要从i+1的位置开始寻找; 
+     * 其实一次寻找之后(i..j)之间肯定为0元素，且j第一次达到末尾就能确定所有非0元素了
+     * Tn = O(n^2)
+     * Sn = O(1)
+     */
+    public void moveZeroesViolently(int[] nums) {
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == 0 && nums[j] != 0) {
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+        }
+    }
+    
     /**
      * 快慢指针： nextNonZeroIndex记录下一个非零索引，遍历数组将非零元素放置nextNonZeroIndex位置并往前移动
      * 当快指针到达末尾时，慢指针之后的元素赋为0
@@ -42,31 +63,7 @@ public class Solution {
                 nextNonZeroIndex++;
              }
         }
-    }
-    public void swap(int[] nums,int a, int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-
-    /**
-     * 暴力解法:  循环两次，将为0的的数依次传递（冒泡）到末尾
-     * Tn = O(n^2)
-     * Sn = O(1)
-     */
-    public void moveZeroesViolently(int[] nums) {
-        int temp = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == 0 && nums[j] != 0) {
-                    temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
-                    break;
-                }
-            }
-        }
-    }
+    } 
 
     private void print(int[] nums) {
         for (int item : nums) {
