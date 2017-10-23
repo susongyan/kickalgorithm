1bee5af14d9170ea71cea0a98171ba86f15e8f4d
minorpoet <bemadoright@gmail.com>
2017-10-23 22:11:23 +0800

    219-containsDuplicate II

---
+++ leetcode/219-containsDuplicate2/ProblmenDescription.md
@@ -0,0 +1,5 @@
+[219. Contains Duplicate II](https://leetcode.com/problems/contains-duplicate-ii/description/)
+
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+给定一个整型数组和一个整数 k，找出是否有2个不同的索引 i 和 j 使得 nums[i] == nums[j] 同时 i 和 j 差值的绝对值不大于k。
---
+++ leetcode/219-containsDuplicate2/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * 主要思路是： 查找表 + 滑动窗口
+ * 通过查找表来减少查找次数，并利用滑动窗口来控制索引间隙
+ * 
+ * Tn = O(n);
+ * Sn = O(n);
+ */
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> slidingSet = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(slidingSet.contains(nums[i]))
+                return true;
+            slidingSet.add(nums[i]);
+            if(slidingSet.size() == k + 1){ // 保持i++之后滑动窗口的长度为 k + 1
+                slidingSet.remove(nums[i - k]); // 移除滑动窗口的第一个元素
+            }
+        }
+        return false;
+    } 
+}
