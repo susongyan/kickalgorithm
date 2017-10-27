9240e359ff586f68368dea49112ba428285cde9b
susy <susongyan@yunquna.com>
2017-10-27 13:20:40 +0800

    206 recursively reverse linkedlist

--- leetcode/206-reverseLinkedLIst/Solution.java
+++ leetcode/206-reverseLinkedLIst/Solution.java
@@ -1,4 +1,5 @@
 class Solution{
+    // iteratively
     public ListNode reverseList(ListNode head){
         // 主要需要注意 覆盖的问题
         ListNode cur = head;
@@ -13,6 +14,19 @@ class Solution{
         return prev;
     }
 
+    // recursively
+    public static ListNode reverseListRecursively(ListNode head) {
+        return reverseListInt(head, null);
+    }
+      
+    private static ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
+    }
+
     /**
      * 节点
      */
@@ -23,5 +37,36 @@ class Solution{
         public ListNode(int x){
             this.val = x;
         }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = generate();
+
+        printLinkedList(node);
+
+        System.out.println();
+        printLinkedList( reverseList(node));
+    }
+
+    private static void printLinkedList(ListNode node) {
+        ListNode cur = node;
+        while (cur != null) {
+            System.out.print(cur.val + " -> ");
+            cur = cur.next;
+        }
+        if (cur == null) {
+            System.out.print("null");
+        }
+    }
+
+    private static ListNode generate() {
+        Random random = new Random();
+        ListNode node = new ListNode(random.nextInt(10));
+        ListNode cur = node;
+        for (int i = 1; i < 5; i++) {
+            cur.next = new ListNode(random.nextInt(10));
+            cur = cur.next;
+        }
+        return node;
     }
 }
