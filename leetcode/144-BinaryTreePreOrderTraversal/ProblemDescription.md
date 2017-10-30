30e69a834579d84faea5dfeaab7369e81068a846
minorpoet <bemadoright@gmail.com>
2017-10-30 22:50:44 +0800

    144-binary tree preorder traversal

---
+++ leetcode/144-BinaryTreePreOrderTraversal/ProblemDescription.md
@@ -0,0 +1,18 @@
+[144. Binary Tree Preorder Traversal](https://leetcode.com/problems/binary-tree-preorder-traversal/description/)
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+
+```
+  1
+    \
+     2
+    /
+   3
+```
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+注意： 递归方案是很简单，你能用迭代来解决么？
---
+++ leetcode/144-BinaryTreePreOrderTraversal/Solution.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 用栈模拟递归的调用顺序
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null){
+            return result;
+        }
+        Stack<Command> stack = new Stack<Command>();
+        stack.push(new Command("go",root));
+        while(stack.size() > 0){
+            Command command = stack.pop();
+
+            if(command.s.equals("print")){
+                result.add(command.node.val);
+            }else{
+                // 根据栈 FILO 倒过来放入(变成中序和后序,只需要改变 push("print",command.node) 的位置)
+                if(command.node.right != null){
+                    stack.push(new Command("go", command.node.right));
+                }
+                if(command.node.left != null){
+                    stack.push(new Command("go", command.node.left));
+                }
+                stack.push(new Command("print",command.node));
+            }
+        }
+        return result;
+    }
+
+    public class Command{
+        String s; //go , print
+        TreeNode node;
+        Command(String s, TreeNode node){
+            this.s = s;
+            this.node = node;
+        }
+    }
+
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x;}
+    }
+}
