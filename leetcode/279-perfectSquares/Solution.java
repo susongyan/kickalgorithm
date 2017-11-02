c5573f9c169a153c1cc903239b1a0f636ca720f2
minorpoet <bemadoright@gmail.com>
2017-11-02 22:20:53 +0800

    make it faster by change search container

--- leetcode/279-perfectSquares/Solution.java
+++ leetcode/279-perfectSquares/Solution.java
@@ -9,9 +9,10 @@
  */
 class Solution {
     public int numSquares(int n) {
-        Queue<Pair<Integer, Integer> queue = new LinkedList<>();
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
         queue.offer(new Pair(n,0));
-
+        boolean[] visited = new boolean[n+1]; // 用数组来做图节点已遍历的标示 O(1)的复杂度 比用Set O(n)的快很多
+        visited[n] = true;
         while(!queue.isEmpty()){
             int num = queue.peek().getKey();
             int step = queue.peek().getValue();
@@ -21,11 +22,25 @@ class Solution {
                 return step;
             }
 
-            for(int i = 1; num - i * i >= 0; i++){
-                queue.offer(new Pair(num - i*i, step+1));
+            for(int i = 1; ; i++){ 
+                /* 
+                * 这边要注意，不要重复已经计算过的点，因为这边要求的最短路径（不包含权重）
+                * 所以下次再回到这个节点的话，步奏肯定比之前的大 
+                */
+                int a = num - i * i ;
+                if(a < 0) 
+                    break;
+                if(a == 0)
+                    return step + 1;
+                if(!visited.[a]){
+                    queue.offer(new Pair(a, step+1));
+                    visited[a] = true;
+                }
             }
         }
+        return 0;
     }
+
 
 
     class Pair<TKey,TValue>{
