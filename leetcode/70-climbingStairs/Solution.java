2b20ace694375f70e4df8f0a24e284e028bb9d7b
susy <susongyan@yunquna.com>
2018-01-02 10:16:43 +0800

    70 climbing stairs

--- leetcode/70-climbingStairs/Solution.java
+++ leetcode/70-climbingStairs/Solution.java
@@ -1,28 +1,28 @@
 package algrithm.climbingstaris;
 
-public class Solution {
+public class Solution {    
     int[] memory;
-
     public int climbStairs(int n) {
-        memory = new int[n + 1];
+        memory = new int[n+1];
         return calculateWays2(n);
     }
 
+
     // 递归
-    public int calculateWays(int n) {
+    public int calculateWays(int n){
         // 当 n = 1 或 0 的时候，可能的走法就只有1种（n==2的话可以走两次1也可以走一次2）
-        if (n == 0 || n == 1) {
+        if(n == 0 || n == 1){
             return 1;
         }
         // if(n==1){
-     
-
-        // if(n==2){
-        //     return 2; 
+        //     return 1;
         // }
-        return calcula
-          
-     
+        // if(n==2){
+        //     return 2;
+        // }
+        return calculateWays(n - 1) + calculateWays(n - 2);
+    }
+ 
     // 动态规划
     public int calculateWays2(int n){
         memory[0] = 1;
