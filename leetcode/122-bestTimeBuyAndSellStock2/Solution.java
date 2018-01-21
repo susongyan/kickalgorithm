16f357c1dd743ee3778e619f800709859130bd4f
minorpoet <bemadoright@gmail.com>
2018-01-21 15:57:40 +0800

    fix index out of bound ex

--- leetcode/122-bestTimeBuyAndSellStock2/Solution.java
+++ leetcode/122-bestTimeBuyAndSellStock2/Solution.java
@@ -5,7 +5,7 @@ public class Solution{
         int res = 0;
         // 如果第二天的股价比第一天高就买入
         for(int i = 0; i < prices.length - 1; i++){
-            res += Math.max(prices[i+1] - prices[i-1], 0); 
+            res += Math.max(prices[i+1] - prices[i], 0); 
         }
         return res;
     }
